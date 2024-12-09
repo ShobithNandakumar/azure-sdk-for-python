76010e6dd8e88f86211245a289dff65e48dd394b
ShobithNandakumar <shobithn8412@gmail.com>
2024-12-09 12:44:43 +0530

    introduce model-proc

---
+++ sdk/ai/azure-ai-inference/azure/ai/inference/router/_patch.py
@@ -0,0 +1,17 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+# pylint: disable=line-too-long,R
+"""Customize generated code here.
+
+Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
+"""
+
+def patch_sdk():
+    """Do not remove from this file.
+
+    `patch_sdk` is a last resort escape hatch that allows you to do customizations
+    you can't accomplish using the techniques described in
+    https://aka.ms/azsdk/python/dpcodegen/python/customize
+    """
