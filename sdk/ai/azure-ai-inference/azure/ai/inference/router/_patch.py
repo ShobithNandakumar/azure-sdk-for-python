bcd3e0e0d85cc39471b5c079c2e5c6cc3e01b572
Vikas Kedia <kediavikas@microsoft.com>
2024-12-11 10:52:50 +0530

    Initial commit of client

--- sdk/ai/azure-ai-inference/azure/ai/inference/router/_patch.py
+++ sdk/ai/azure-ai-inference/azure/ai/inference/router/_patch.py
@@ -7,7 +7,30 @@
 
 Follow our quickstart for examples: https://aka.ms/azsdk/python/dpcodegen/python/customize
 """
+import abc
+from azure.ai.inference import (
+    ChatCompletionsClient,
+    EmbeddingsClient,
+)
 
+from azure.ai.inference.aio import (
+    ChatCompletionsClient as AsyncChatCompletionsClient,
+    EmbeddingsClient as AsyncEmbeddingsClient,
+)
+from azure.ai.inference.models import (
+    ChatCompletions,
+    StreamingChatCompletions,
+    AsyncStreamingChatCompletions,
+)
+
+class ModelConfig(abc.ABC):
+    @abc.abstractmethod
+    def 
+class ModelRouter(abc.ABC):
+    @abc.abstractmethod
+    def complete(self) -> ChatCompletions:
+        pass
+    
 def patch_sdk():
     """Do not remove from this file.
 
